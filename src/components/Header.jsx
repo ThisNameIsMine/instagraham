e8af81e086c16437c619020dd11ee038b3de3943
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-15 17:08:01 +0200

    Add more to header: search bar.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import Image from "next/image";
+import {
+  MagnifyingGlassCircleIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Header() {
   return (
-    <div className="">
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            className="object-contain"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-            alt="Ig logo"
-            fill={true}
-          />
-        </div>
-        <div className="cursor-pointer h-10 w-10 relative lg:hidden">
-          <Image
-            className="object-contain"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png"
-            alt="Ig logo"
-            fill={true}
-          />
-        </div>
-        <h2>Right sides</h2>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          className="object-contain"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt="Ig logo"
+          fill={true}
+        />
       </div>
-
+      <div className="cursor-pointer h-10 w-10 relative lg:hidden">
+        <Image
+          className="object-contain"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png"
+          alt="Ig logo"
+          fill={true}
+        />
+      </div>
       {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm rounded-md focus:ring-black focus:border-black"
+        />
+      </div>
       {/* Right */}
+      <h2>Right sides</h2>
     </div>
   );
 }

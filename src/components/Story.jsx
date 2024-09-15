257a4e00f5c8fbbe644bda09f062f67b27aa5b7d
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-15 21:43:42 +0200

    Put stories to one div, scrollable, removed scrollbar

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function Story({ img, username }) {
   return (
     <div className="p-2">
-      <img src={img} alt={username} />
-      <p className="text-red-900 h-3">{username}</p>
+      <img
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        src={img}
+        alt={username}
+      />
+      <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
 }

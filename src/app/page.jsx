f0b17aedc6c8abce5aa9d3b583dc886a2b5f911e
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-15 16:26:33 +0200

    Create skeleton for instagraham.

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <title>Home</title>
+      <meta name="description" content="Home page" />
+      <link rel="icon" href="/favicon.ico" />
+      <div className="">Home</div>
+      {/* Header */}
+      {/* Feed */}
+      {/* Modal */}
+    </div>
+  );
+}
